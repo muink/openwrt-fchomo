7eb26d5ac38a852f3018b0b24678f1854f10bf48
Anya Lin <hukk1996@gmail.com>
2024-10-01 23:31:35 +0800

    chore: fixes and improvements

--- luci-app-fchomo/.prepare.sh
+++ luci-app-fchomo/.prepare.sh
@@ -9,6 +9,7 @@ OS=linux
 ARCH=amd64
 JQVERSION=1.7.1
 DOCNAME=Ruleset-URI-Scheme
+SHARKNAME=shark-taiko.gif
 
 mkdir -p "$PKG_BUILD_BIN"
 curl -L "https://github.com/jqlang/jq/releases/download/jq-${JQVERSION}/jq-${OS}-${ARCH}" -o "$PKG_BUILD_BIN"/jq
@@ -33,6 +34,8 @@ tail -n +$(( $p +1 )) $DOCNAME.html
 popd
 minify "$PKG_BUILD_DIR"/buildin.html | base64 | tr -d '\n' > "$PKG_BUILD_DIR"/base64
 sed -i "s|'cmxzdHBsYWNlaG9sZGVy'|'$(cat "$PKG_BUILD_DIR"/base64)'|" "$PKG_BUILD_DIR"/htdocs/luci-static/resources/fchomo.js
+base64 "$CURDIR"/docs/img/$SHARKNAME | tr -d '\n' > "$PKG_BUILD_DIR"/base64
+sed -i "s|'c2hhcmstdGFpa28uZ2lm'|'$(cat "$PKG_BUILD_DIR"/base64)'|" "$PKG_BUILD_DIR"/htdocs/luci-static/resources/fchomo.js
 
 if [ -d "$CURDIR/.git" ]; then
 	config="$CURDIR/.git/config"

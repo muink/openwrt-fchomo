22a72a55fb88085f8aaa4da17c2c800bd6faafaa
Anya Lin <hukk1996@gmail.com>
2024-10-04 15:39:48 +0800

    chore: fixes and improvements

--- luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/hosts.js
+++ luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/hosts.js
@@ -28,6 +28,7 @@ return view.extend({
 			E('p', { 'class': 'cbi-section-descr' }, _('Custom internal hosts. Support <code>yaml</code> or <code>json</code> format.')),
 			E('p', {}, E('textarea', {
 				'class': 'cbi-input-textarea',
+				'placeholder': "hosts:\n  '*.clash.dev': 127.0.0.1\n  'alpha.clash.dev': '::1'\n  test.com: [1.1.1.1, 2.2.2.2]\n  baidu.com: google.com",
 				'style': 'width:100%;font-family:' + hm.monospacefonts.join(','),
 				'rows': 25,
 				'disabled': isReadonlyView

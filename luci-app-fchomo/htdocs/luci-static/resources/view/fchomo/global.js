e2feb1c470e4a9a4885bbecc91dbcb6a40d7a2cf
Anya Lin <hukk1996@gmail.com>
2024-10-04 17:20:22 +0800

    chore: fixes and improvements

--- luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/global.js
+++ luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/global.js
@@ -557,14 +557,14 @@ return view.extend({
 		/* Routing control */
 		ss.tab('routing_control', _('Routing Control'));
 
-		so = ss.taboption('routing_control', form.Value, 'routing_tcpport', _('Routing TCP ports'),
+		so = ss.taboption('routing_control', form.Value, 'routing_tcpport', _('Routing ports') + ' (TCP)',
 			_('Specify target ports to be proxied. Multiple ports must be separated by commas.'));
 		so.value('', _('All ports'));
 		so.value('common', _('Common ports only (bypass P2P traffic)'));
 		so.value('common_stun', _('Common and STUN ports'));
 		so.validate = L.bind(hm.validateCommonPort, so);
 
-		so = ss.taboption('routing_control', form.Value, 'routing_udpport', _('Routing UDP ports'),
+		so = ss.taboption('routing_control', form.Value, 'routing_udpport', _('Routing ports') + ' (UDP)',
 			_('Specify target ports to be proxied. Multiple ports must be separated by commas.'));
 		so.value('', _('All ports'));
 		so.value('common', _('Common ports only (bypass P2P traffic)'));
